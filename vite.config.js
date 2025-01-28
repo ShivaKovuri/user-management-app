60e9fc28524a3e065c2dd7d8cef6ea6a68b49aa0
ShivaKovuri <shivaprasad454@gmail.com>
2025-01-28 14:57:49 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/user-management-app/",
   plugins: [react()],
+  base: "/user-management-app/",
 });
